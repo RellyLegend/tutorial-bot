6884a939e0cc0fb291fa042d4c65758a97cc82bd
RellyLegend <75770996+RellyLegend@users.noreply.github.com>
2021-08-12 11:02:14 +0100

    Update index.js
    Updating index to episode 2 code.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Importing modules and files.
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, MessageEmbed } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 // We'll use config.json to configure some stuff.
 // We'll define prefix, but use it on the next video.
@@ -9,16 +9,32 @@ const { token, prefix } = require('./config.json');
 client.on('ready', () => {
     // Sends a message in our console/terminal when the bot is ready and logged into Discord via bot token.
     console.log(`${client.user.tag} is logged into Discord.`)
+    // This will be done in the ready event.
+
+   client.user.setActivity(`YouTube`, { type: "WATCHING"})
 });
 
 // Let's create a command.
 
-client.on('messageCreate', message => {
-    if (message.content === `ping`) {
-        message.channel.send(`Pong!`)
-    }
-});
+client.on("messageCreate", message => {
+    // Start with defining args!
+    const args = message.content.slice(prefix.length).trim().split(" ");
+    const command = args.shift().toLowerCase();
 
+    if (!message.content.startsWith(prefix)) return // Makes sure the command starts with a prefix.
+
+ // Our prefix is in config. Defined here.
+   if (command === `ping`) {
+       message.channel.send(`Ping: **${client.ws.ping}ms**`) // Returns ping as response.
+   } else if (command === `say`) {
+       // With the help of args, we can make a say command!
+       const say = args.join(" ");
+       if (!say) return message.channel.send(`Please input a text to say.`) // Makes sure there is a text for the bot to say.
+
+       message.channel.send(say);
+   }
+});
+// Note: All the code in this video will be in my GitHub.
 // Login to our discord bot.
 client.login(token) // token = config.token refering to our bot's token.
 
